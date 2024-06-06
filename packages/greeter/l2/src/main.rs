f4960dc0d3d7a05049f74c4163ba9e175f3caa67
TucksonDev <tucksondev@gmail.com>
2024-06-06 16:30:54 +0100

    Update to SDK 0.5.0

--- packages/greeter/l2/src/main.rs
+++ packages/greeter/l2/src/main.rs
@@ -3,9 +3,7 @@
 //! that update their values using cross-chain messages
 //!
 //! This file is used to export the ABI of the contract
-#![cfg_attr(not(feature = "export-abi"), no_main)]
-
 #[cfg(feature = "export-abi")]
 fn main() {
-    stylus_greeter_l2::main();
+    stylus_greeter_l2::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
 }
